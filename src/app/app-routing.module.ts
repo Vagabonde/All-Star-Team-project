a206e2c4a83704a293afb382a5c9f78cefbe38ce
Maria <notherndeer21@gmail.com>
2018-10-24 20:33:28 +0300

    Created template for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GroupsComponent} from "./groups/groups.component";
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {GroupComponent} from "./group/group.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path:'groups', component: GroupsComponent},
+    {path:'groups/:groupId', component: GroupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupsComponent } from './groups/groups.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { GroupComponent } from './group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupsComponent,
+    LoginComponent,
+    HomeComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
