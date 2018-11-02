6d16a905baf286faad4d527fd58d0f096c29f3fa
OlejukM <olejukmaslo@gmail.com>
2018-11-02 19:17:16 +0200

    unused variables were deleted and default rout was changed to Homepage

--- src/app/group/timeline/timeline.component.ts
+++ src/app/group/timeline/timeline.component.ts
@@ -11,8 +11,6 @@ import { TASKS } from './mock-tasks'
 export class TimelineComponent implements OnInit {
   tasks = TASKS;
   selectedTask: Task;
-  selectedInfo: Task;
-  tabs = 'Hello';
 
   onSelect(currentTask: Task): void {
     if(this.selectedTask === currentTask){
