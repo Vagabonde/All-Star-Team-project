545854d3e87adaa27590912ee2dc3eb743afcb21
OlejukM <olejukmaslo@gmail.com>
2018-11-09 16:50:38 +0200

    double quotes were changed to single quotes

--- src/app/homework-tab/homework-tab.component.ts
+++ src/app/homework-tab/homework-tab.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Task } from "../interface/task";
-import { TASKS } from "../shared/mocks/mock-tasks";
+import { Task } from '../interface/task';
+import { TASKS } from '../shared/mocks/mock-tasks';
 
 
 @Component({
