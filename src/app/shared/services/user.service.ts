3acdb24cf846ccee2fad499ef46fe72999c00d0d
Maria <notherndeer21@gmail.com>
2018-11-22 13:41:04 +0200

    created separate PR for user service

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -13,66 +13,73 @@ const key: string = 'users';
 })
 export class UserService {
 
-  constructor(public mockService: MockService) {}
-
-  users = this.mockService.storage.get(key);
-
-  public getUsers(): Observable<User[]> {
-      return of(this.users);
-  }
-
-  public getStudents(): Observable<User[]> {
-      let students = this.users.filter(user => !user.isAdmin);
-      return of(students);
-  }
-
-  public getCuratorByGroupId(groupId: string): Observable<User> {
-      let curator = this.users.filter(user => user.isAdmin && user.groupId === groupId)[0]; //here could be array of curators
-      return of(curator);
-  }
-
-  public getStudentsByGroupId(groupId: string): Observable<User[]> {
-      let students = this.users.filter(user => !user.isAdmin && user.groupId === groupId);
-      return of(students);
-  }
-
-  public getUserById(userId: string): Observable<User> {
-    let user = this.users.filter(u => u.id === userId )[0];
-    return of(user)
-  }
-
-  public setStudentGroup(student: User, currentGroupId: string): void {
-    for (let i = 0; i < this.users.length; i++) {
-      if (this.users[i].id === student.id) {
-        this.users[i].groupId = currentGroupId;
-      }
+    constructor(public mockService: MockService) {
     }
-  }
 
-  public addUserLesson(userId: string, lesson: UserLesson): void {
+    users = this.mockService.storage.get(key);
 
-    for(let i = 0; i < this.users.length; i++) {
-      if (this.users[i].id === userId) {
-        this.users[i].lessons.forEach( (item, index) => {
-          if (item.lessonId === lesson.lessonId) {
-            this.users[i].lessons.splice(index, 1);
-          }
-        });
-        this.users[i].lessons.push(lesson);
-      }
+    public getUsers(): Observable<User[]> {
+        return of(this.users);
     }
-  }
 
-  public addUserHomework(userId: string, newHomework: Homework, currentLesson: Lesson) {
-    for(let i = 0; i < this.users.length; i++) {
-      if (this.users[i].id === userId) {
-        for (let j = 0; j < this.users[i].lessons.length; j++) {
-          if(this.users[i].lessons[j].lessonId === currentLesson.id) {
-            this.users[i].lessons[j].homework = newHomework;
-            console.log(this.users[i]);
-          }
+    public getStudents(): Observable<User[]> {
+        let students = this.users.filter(user => !user.isAdmin);
+        return of(students);
+    }
+
+    public getCuratorByGroupId(groupId: string): Observable<User> {
+        let curator = this.users.filter(user => user.isAdmin && user.groupId === groupId)[0]; //here could be array of curators
+        return of(curator);
+    }
+
+    public getStudentsByGroupId(groupId: string): Observable<User[]> {
+        let students = this.users.filter(user => !user.isAdmin && user.groupId === groupId);
+        return of(students);
+    }
+
+    public getUserById(userId: string): Observable<User> {
+        let user = this.users.filter(u => u.id === userId)[0];
+        return of(user)
+    }
+
+    private updateStorage(key, value) {
+        this.mockService.storage.set(key, value);
+    }
+
+    public setStudentGroup(student: User, currentGroupId: string): void {
+        for (let i = 0; i < this.users.length; i++) {
+            if (this.users[i].id === student.id) {
+                this.users[i].groupId = currentGroupId;
+            }
         }
-      }
+        this.updateStorage(key, this.users);
     }
-  }
+
+    public addUserLesson(userId: string, lesson: UserLesson): void {
+
+        for (let i = 0; i < this.users.length; i++) {
+            if (this.users[i].id === userId) {
+                this.users[i].lessons.forEach((item, index) => {
+                    if (item.lessonId === lesson.lessonId) {
+                        this.users[i].lessons.splice(index, 1);
+                    }
+                });
+                this.users[i].lessons.push(lesson);
+            }
+        }
+        this.updateStorage(key, this.users);
+    }
+
+    public addUserHomework(userId: string, newHomework: Homework, currentLesson: Lesson) {
+        for (let i = 0; i < this.users.length; i++) {
+            if (this.users[i].id === userId) {
+                for (let j = 0; j < this.users[i].lessons.length; j++) {
+                    if (this.users[i].lessons[j].lessonId === currentLesson.id) {
+                        this.users[i].lessons[j].homework = newHomework;
+                    }
+                }
+            }
+        }
+        this.updateStorage(key, this.users);
+    }
 }
